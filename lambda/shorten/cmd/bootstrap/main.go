cac32628fb0d46298235d112fd92aaec0675b4fd
Francesco Galisi <francesco.galisi@imagicle.com>
2024-02-26 02:09:08 +0100

    Add an api gateway with lambda integration

--- lambda/shorten/cmd/bootstrap/main.go
+++ lambda/shorten/cmd/bootstrap/main.go
@@ -5,19 +5,21 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
-}
-
-func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
+func HandleRequest(ctx context.Context, event *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	if event == nil {
 		return nil, fmt.Errorf("received nil event")
 	}
-	message := fmt.Sprintf("Hello %s! _HANDLER=%s", event.Name, os.Getenv("_HANDLER"))
-	return &message, nil
+
+	message := fmt.Sprintf("Hello World! _HANDLER=%s; %+v", os.Getenv("_HANDLER"), *event)
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       message,
+	}, nil
 }
 
 func main() {
--- lib/link-a-boo-stack.ts
+++ lib/link-a-boo-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from "aws-cdk-lib";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -7,10 +8,18 @@ export class LinkABooStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const api = new apigateway.RestApi(this, "link-a-boo-api", {
+      restApiName: "Link-a-boo api",
+    });
+
     const handler = new lambda.Function(this, "root-handler-lambda", {
       runtime: lambda.Runtime.PROVIDED_AL2,
       code: lambda.Code.fromDockerBuild("lambda/shorten"),
       handler: "shorten",
     });
+
+    const shortenIntegration = new apigateway.LambdaIntegration(handler);
+
+    api.root.addMethod("GET", shortenIntegration);
   }
 }

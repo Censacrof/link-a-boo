f4efbb0739393399f587d4abbaec3abcf9a8cf98
Francesco Galisi <francesco.galisi@imagicle.com>
2024-03-03 00:38:45 +0100

    Improved lambda file stucture

--- lambda/go/cmd/shorten/main.go
+++ lambda/go/cmd/shorten/main.go
@@ -1,8 +1,82 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"net/url"
+
+	"github.com/Censacrof/link-a-boo/lambda/shorten/pkg/db"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
+
+type ShortenRequest struct {
+	TargetUrl string `json:"targetUrl"`
+}
+
+type ShortenResponse struct {
+	ShortenedUrl string `json:"shortenedUrl"`
+}
+
+func HandleShortenRequest(ctx context.Context, event *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       fmt.Sprintf("Can't load default configuration: %v", err),
+		}, nil
+
+	}
+
+	var shortenRequest ShortenRequest
+	err = json.Unmarshal([]byte(event.Body), &shortenRequest)
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Invalid request",
+		}, nil
+	}
+
+	targetUrl, err := url.Parse(shortenRequest.TargetUrl)
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Invalid targetUrl",
+		}, nil
+	}
+
+	shortenedUrl := db.NewShortenedUrl(*targetUrl)
+	ddbClient := dynamodb.NewFromConfig(cfg)
+
+	err = shortenedUrl.Put(ctx, *ddbClient)
+
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       fmt.Sprintf("Internal server error: %v", err),
+		}, nil
+	}
+
+	resp := ShortenResponse{
+		ShortenedUrl: shortenedUrl.Slug,
+	}
+
+	respBody, err := json.Marshal(resp)
+	if err != nil {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       "Internal server error",
+		}, nil
+	}
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(respBody),
+	}, nil
+}
 
 func main() {
 	lambda.Start(HandleShortenRequest)
---
+++ lambda/go/pkg/db/shortened_url.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/google/uuid"
+)
+
+type ShortenedUrl struct {
+	Slug string `dynamodbav:"slug"`
+	Url  string `dynamodbav:"url"`
+}
+
+func NewShortenedUrl(url url.URL) ShortenedUrl {
+	slug := uuid.New().String()
+
+	return ShortenedUrl{
+		Slug: slug,
+		Url:  url.String(),
+	}
+}
+
+func (self *ShortenedUrl) Put(ctx context.Context, client dynamodb.Client) error {
+	item, err := attributevalue.MarshalMap(self)
+
+	if err != nil {
+		return fmt.Errorf("Can't add url to db: %w", err)
+	}
+
+	tableName := os.Getenv("URLS_TABLE_NAME")
+
+	_, err = client.PutItem(ctx, &dynamodb.PutItemInput{
+		Item:      item,
+		TableName: aws.String(tableName),
+	})
+
+	if err != nil {
+		return fmt.Errorf("Put in table '%s' failed: %w", tableName, err)
+	}
+
+	return nil
+}
